5da5a5220e4f83443d38723ed588cbb340f95a99
IsaoHajime <hajime@darlingisbeautiful.com>
2024-11-19 17:30:21 +0900

    matplotlib is comment

--- pages/page_3.py
+++ pages/page_3.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 # データ分析関連
 df = pd.read_csv('./data/平均気温.csv', index_col='年月')
@@ -9,7 +9,7 @@ st.line_chart(df)
 st.bar_chart(df['月平均'])
 
 # matplotlib
-fig, ax = plt.subplots()
-ax.plot(df.index, df['月平均'])
-ax.set_title('matplotlib Graph')
-st.pyplot(fig)
+#fig, ax = plt.subplots()
+#ax.plot(df.index, df['月平均'])
+#ax.set_title('matplotlib Graph')
+#st.pyplot(fig)
